e16bf3cfad87fa2febda9674dcc362d1a2105f5f
DanielBryjak <d.bryjak@op.pl>
2022-10-25 20:16:54 +0200

    For - pętla powtarzanie

---
+++ src/Foreach.java
@@ -0,0 +1,8 @@
+public class Foreach {
+    public static void main(String[] args) {
+        String [] catTable=new String[] {"Kola","Kita","Kazia","Zygfryd"};
+        for (String cat: catTable){
+            System.out.println(cat);
+        }
+    }
+}
