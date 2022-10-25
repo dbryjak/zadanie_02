af9bd5a8f10a25f3f767caa9c27c555fc4cdb859
DanielBryjak <d.bryjak@op.pl>
2022-10-25 21:03:13 +0200

    Domowa -  praca

---
+++ src/Domowe.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Domowe {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Hasło");
+        String imie = sc.next();
+        System.out.println("żonaty");
+        boolean stanCywilny = sc.nextBoolean();
+
+    }
+    }
+}
